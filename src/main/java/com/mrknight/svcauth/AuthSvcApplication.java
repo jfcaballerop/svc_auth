4967e238cd33e29f592342ecbedf25f4e0f71491
jfcaballero <jfcp@inesingenieros.com>
2019-07-02 13:44:18 +0200

    add swagger

--- src/main/java/com/mrknight/svcauth/AuthSvcApplication.java
+++ src/main/java/com/mrknight/svcauth/AuthSvcApplication.java
@@ -15,7 +15,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class AuthSvcApplication {
 
 	@Autowired
--- src/main/java/com/mrknight/svcauth/security/jwt/SecurityConfig.java
+++ src/main/java/com/mrknight/svcauth/security/jwt/SecurityConfig.java
@@ -34,13 +34,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.cors().and().csrf().disable()
-        .authorizeRequests().antMatchers("/h2-console/**").permitAll().anyRequest()
-        .authenticated()
-        .and()
-        .addFilter(new JWTAuthenticationFilter(authenticationManager()))
-        .addFilter(new JWTAuthorizationFilter(authenticationManager()))
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll().anyRequest()
+                .authenticated().and().addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager())).sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.headers().frameOptions().disable();
 
